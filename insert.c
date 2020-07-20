314cc2f90ae6b9b0bdd2ddcc589e51dec5aa4eae
peter1 <hans_peter.meyer@yahoo.de>
2020-07-20 11:09:22 +0200

    Create insert.c
    I cant type any Digit in it.
    I cant find my mistake.

---
+++ insert.c
@@ -0,0 +1,10 @@
+//Example 1
+#include <stdio.h>
+int main()
+{
+  int number;
+  printf("Enter a Number: ");
+    scanf("%d",&number);	//stored input in variable and call it number
+  printf("Cube of number is: %d",number*number*)
+  return 0;
+}
